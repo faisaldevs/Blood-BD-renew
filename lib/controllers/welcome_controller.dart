e5060d0653a9b09cb0df10f50700ab43aafae975
Md Faisal Ahamed <faisalah331@gmail.com>
2024-03-05 11:40:35 +0600

    home

--- lib/controllers/welcome_controller.dart
+++ lib/controllers/welcome_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:blood_bd/screens/user_auth/login_screen/login_screen.dart';
 import 'package:get/get.dart';
 
@@ -10,5 +12,18 @@ class WelcomeController extends GetxController {
 
   loginBtn() async {
     Get.to(const LoginScreen());
+  }
+
+  List<String> imageUrls = [
+    "assets/welcome_page/bd1.png",
+    "assets/welcome_page/bd2.png",
+    "assets/welcome_page/bd3.png",
+    "assets/welcome_page/bd4.png",
+  ];
+
+  RxInt currentIndex = 1.obs;
+
+  void changeBackground() {
+    currentIndex.value = Random().nextInt(imageUrls.length);
   }
 }
--- lib/screens/user_auth/welcome_screen.dart
+++ lib/screens/user_auth/welcome_screen.dart
@@ -13,70 +13,69 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Obx(
-      () => GestureDetector(
-        onTap: welcomeController.updateImage,
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(
-                    welcomeController.currentImageUrl.value,
-                  ),
-                  fit: BoxFit.cover)),
-          child: Container(
-            margin: const EdgeInsets.only(bottom: 15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(
-                  width: Get.width * .4,
-                  height: Get.height * .06,
-                  child: CustomButton(
-                    onPressed: () {
-                      // print("pressed");
-                      welcomeController.signupBtn();
-                    },
-                    child: Text(
-                      'Sign up',
-                      style:
-                          GoogleFonts.roboto(color: Colors.white, fontSize: 16),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                const Text(
-                  "or",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                SizedBox(
-                  width: Get.width * .4,
-                  height: Get.height * .06,
-                  child: CustomButton(
-                    onPressed: () {
-                      // print("pressed");
-                      welcomeController.loginBtn();
-                    },
-                    child: Text(
-                      'Login',
-                      style: GoogleFonts.roboto(
-                        color: Colors.white,
-                        fontSize: 16,
+        body: Obx(() => GestureDetector(
+            onTap: welcomeController.changeBackground,
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        welcomeController
+                            .imageUrls[welcomeController.currentIndex.value],
+                      ),
+                      fit: BoxFit.cover)),
+              child: Container(
+                margin: const EdgeInsets.only(bottom: 15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      width: Get.width * .4,
+                      height: Get.height * .06,
+                      child: CustomButton(
+                        onPressed: () {
+                          // print("pressed");
+                          welcomeController.signupBtn();
+                        },
+                        child: Text(
+                          'Sign up',
+                          style: GoogleFonts.roboto(
+                              color: Colors.white, fontSize: 16),
+                        ),
                       ),
                     ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    ));
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const Text(
+                      "or",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    SizedBox(
+                      width: Get.width * .4,
+                      height: Get.height * .06,
+                      child: CustomButton(
+                        onPressed: () {
+                          // print("pressed");
+                          welcomeController.loginBtn();
+                        },
+                        child: Text(
+                          'Login',
+                          style: GoogleFonts.roboto(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ))));
   }
 }
